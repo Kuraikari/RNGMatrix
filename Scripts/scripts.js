c70d96b559a2306b3084631b8129c12ad6ef90e6
Zian Wermelinger <z.wermelinger@computech.ch>
2024-06-07 17:08:05 +0200

    FIX: imports and linking

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,1 +1,12 @@
-require("nunjucks");
+function fetchData() {
+    fetch("http://localhost:3000")
+        .then(data => {
+            console.debug(data);
+            return data;
+        })
+        .catch(reason => console.error(reason));
+}
+
+function createData() {
+
+}
