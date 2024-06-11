888c1ba54cadd3a0f7c53925579794763840ab7d
Zian Wermelinger <z.wermelinger@computech.ch>
2024-06-11 10:37:38 +0200

    ENH: create an example env file and fix names in app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const backend = require("./app/server/backend.js");
 const frontend = require("./app/frontend/frontend.js");
 
-const backendPort = process.env.BACKENDPORT || 3000;
-const frontendPort = process.env.FRONTENDPORT || 3001;        // set our port
+const backendPort = process.env.APP_BACKENDPORT || 3000;
+const frontendPort = process.env.APP_FRONTENDPORT || 3001;        // set our port
 
 backend.listen(backendPort);
 console.log('Backend happens on port ' + backendPort);
