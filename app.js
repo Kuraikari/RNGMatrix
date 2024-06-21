4c8e632bd0afd241c18c94067d8a964a9912d79e
Zian Wermelinger <z.wermelinger@computech.ch>
2024-06-21 16:56:12 +0200

    ENH: Better DX (developer experience) by putting the localhost url in the console during start up

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const backendPort = process.env.APP_BACKENDPORT || 3000;
 const frontendPort = process.env.APP_FRONTENDPORT || 3001;        // set our port
 
 backendListen.listen(backendPort);
-console.log((new Date()).toLocaleTimeString() + " $ " + 'Backend happens on port ' + backendPort);
+console.log((new Date()).toLocaleTimeString() + " $ " + 'Backend: http://localhost:' + backendPort);
 
 frontendListen.listen(frontendPort);
-console.log((new Date()).toLocaleTimeString() + " $ " + 'Frontend happens on port ' + frontendPort);
+console.log((new Date()).toLocaleTimeString() + " $ " + 'Frontend: http://localhost:' + frontendPort);
