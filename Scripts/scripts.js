6d694d581f00f7ece65d3b280d18e6357ee3b7aa
Zian Wermelinger <z.wermelinger@computech.ch>
2024-06-24 14:14:29 +0200

    ENH: create generate random instrumental string for udio instrumental hack

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,54 +1,70 @@
-import SongModel from "./models/SongModel.js";
-
-/**
- * Fetches the current status of the backend
- * @returns json of retrieved status message
- */
-async function fetchStatus() {
-    try {
-        const re = await fetch("http://localhost:3000/api/status");
-        return re.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-/**
- * Fetches data from the backend using the api for retrieving songs.
- * @returns {string} json of retrieved songs
- */
-async function fetchData() {
-    try {
-        const re = await fetch("http://localhost:3000/api/songs");
-        return re.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
-
 /**
  * 
- * @param {SongModel} data Model for the song data
- * @returns {string} the response as a json string
+ * @param {number} numChars 
+ * @param {{usePnct: boolean, useSpecial: boolean, useRandAll: boolean}} settings 
+ * @returns 
  */
-async function createData(data) {
-    try {
-        const resp = await fetch('http://localhost:3000/api/song', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        
-        const content = await resp.json();
-        return content;
-    } catch (e) {
-        console.error(e);
+function generateRandomInstrumentalString(numChars, settings) {
+    function getRandomInt(min, max) {
+        return Number.parseInt(Math.round(Math.random() * (max - min) + min));
     }
+
+    /**
+     * 
+     * @param {Array<any>} arr 
+     */
+    function shuffle(arr) {
+        let currIndex = arr.length;
+
+        while (currIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currIndex);
+            currIndex--;
+
+            [arr[currIndex], arr[randomIndex]] = [arr[randomIndex], arr[currIndex]];
+        }
+    }
+
+    var str = "";
+    const listOfChars = [];
+
+    if (settings.usePnct) {
+        listOfChars.push(33);
+        listOfChars.push(44);
+        listOfChars.push(45);
+        listOfChars.push(46);
+        listOfChars.push(58);
+        listOfChars.push(59);
+        listOfChars.push(63);
+    }
+
+    if (settings.useSpecial) {
+        listOfChars.push(34);
+        listOfChars.push(35);
+        listOfChars.push(36);
+        listOfChars.push(37);
+        listOfChars.push(38);
+        listOfChars.push(39);
+        listOfChars.push(42);
+        listOfChars.push(43);
+        listOfChars.push(47);
+        listOfChars.push(60);
+        listOfChars.push(61);
+        listOfChars.push(62);
+    }
+
+    for (let i = 0; i < numChars; i++) {
+        shuffle(listOfChars);
+        str += String.fromCharCode(listOfChars.at(getRandomInt(0, listOfChars.length - 1)));
+    }
+    
+    return str;
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    
+    const gem = generateRandomInstrumentalString(10, {
+        usePnct: true,
+        useSpecial: true 
+    });
+
+    console.debug(gem);
 });
