46d9505b050dacd198858233df27e5cdec8d9558
Kuraikari <zwermi@gmail.com>
2024-09-17 22:16:54 +0200

    FEA: add audio tag and replace empty canvas with image from metadata + show lyrics on audio play

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // BASE SETUP
 // =============================================================================
+import dotenv from 'dotenv';
 import clc from 'cli-color';
 import { backendListen } from "./app/server/backend.js";
 import { frontendListen } from "./app/frontend/frontend.js";
 
+dotenv.config();
+
 const backendPort = process.env.APP_BACKENDPORT || 3000;
-const frontendPort = process.env.APP_FRONTENDPORT || 3001;        // set our port
+const frontendPort = process.env.APP_FRONTENDPORT || 3001;
+const backendUrl = process.env.APP_BACKENDURL || 'http://localhost:';
+const frontendUrl = process.env.APP_FRONTENDURL || 'http://localhost:';
 
 backendListen.listen(backendPort);
 
